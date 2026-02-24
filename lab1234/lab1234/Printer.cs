5dde0b23e4be0c7c29e5ee26bd4e5a53308711bf
wrknbl <inevatovmaximst@yandex.ru>
2026-02-24 09:56:47 +0400

    2

--- lab1234/lab1234/Printer.cs
+++ lab1234/lab1234/Printer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab1_Printer
+﻿namespace Lab1_Printer
 {
 	public class Printer
 	{
