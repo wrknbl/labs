2264eb70c22d6142278ea5091b69f759bfa82a2e
Студент <marisendikaeva@mail.ru>
2026-03-11 13:29:20 +0300

    gotovo

--- lab1234/lab1234/Network.cs
+++ lab1234/lab1234/Network.cs
@@ -9,18 +9,16 @@ namespace lab1234
     public class Network<T> where T : IConnectable
     {
         private List<T> _devices;
-        private static int _totalDevicesInAllNetworks = 0; // статическое поле (лекция 3, стр. 2-5)
+        private static int _totalDevicesInAllNetworks = 0;
 
         public Network(IEnumerable<T> devices)
         {
             _devices = new List<T>(devices);
-            _totalDevicesInAllNetworks += _devices.Count; // увеличиваем счётчик при создании сети
+            _totalDevicesInAllNetworks += _devices.Count;
         }
 
-        // Статическое свойство для доступа к счётчику
         public static int TotalDevicesInAllNetworks => _totalDevicesInAllNetworks;
 
-        // Метод для подключения всех устройств
         public void ConnectAll()
         {
             Console.WriteLine($"\nПодключение всех устройств в сети (всего {_devices.Count})");
@@ -33,7 +31,6 @@ namespace lab1234
             }
         }
 
-        // Метод для отключения всех устройств
         public void DisconnectAll()
         {
             Console.WriteLine($"\nОтключение всех устройств в сети");
@@ -46,20 +43,16 @@ namespace lab1234
             }
         }
 
-        // Метод, возвращающий список подключённых устройств
         public List<T> GetConnectedDevices()
         {
             return _devices.Where(d => d.IsConnected).ToList();
         }
 
-        // Для удобства выведем информацию о всех устройствах в сети
         public void PrintAllDevicesInfo()
         {
             Console.WriteLine($"\nИнформация об устройствах в сети (всего {_devices.Count})");
             foreach (var device in _devices)
             {
-                // Поскольку T : IConnectable, но не обязательно Device, используем GetInfo если есть,
-                // либо просто ToString. Для наших классов это сработает.
                 Console.WriteLine(device is Device d ? d.GetInfo() : device.ToString());
             }
         }
